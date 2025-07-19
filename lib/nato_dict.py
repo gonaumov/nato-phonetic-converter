1c3ddc549b21fc362c727ce35b776ad8872b10fa
Georgi Naumov <gonaumov@gmail.com>
2025-07-19 18:31:30 +0300

    Commiting lib directory

---
+++ lib/nato_dict.py
@@ -0,0 +1,32 @@
+class NatoDict(dict):
+    def __missing__(self, key):
+        return key # return key itself if not found
+
+nato_alphabet = NatoDict({
+    'A': 'Alfa',
+    'B': 'Bravo',
+    'C': 'Charlie',
+    'D': 'Delta',
+    'E': 'Echo',
+    'F': 'Foxtrot',
+    'G': 'Golf',
+    'H': 'Hotel',
+    'I': 'India',
+    'J': 'Juliett',
+    'K': 'Kilo',
+    'L': 'Lima',
+    'M': 'Mike',
+    'N': 'November',
+    'O': 'Oscar',
+    'P': 'Papa',
+    'Q': 'Quebec',
+    'R': 'Romeo',
+    'S': 'Sierra',
+    'T': 'Tango',
+    'U': 'Uniform',
+    'V': 'Victor',
+    'W': 'Whiskey',
+    'X': 'X-ray',
+    'Y': 'Yankee',
+    'Z': 'Zulu'
+})
